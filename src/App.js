9c976fe515a312f7c7c39bace3cd7d0ffdbb1e5b
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-17 16:59:58 +0200

    first commit mouseMove log react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react';
+import reactLogo from "./svg/react-logo.svg";
+
+const MouseTracker = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const handleOnMouseMove = (e) => {
+    /* update mouse position */
+    setPosition({x:e.clientX, y:e.clientY});
+  };
+ 
+  return (
+    <header className="App-header" onMouseMove={handleOnMouseMove}>
+      <img
+        src={reactLogo}
+        className="App-logo"
+        alt="logo"
+        style={{ position: "absolute" , left:position.x, top:position.y }}
+       
+      />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <MouseTracker />
     </div>
   );
 }
